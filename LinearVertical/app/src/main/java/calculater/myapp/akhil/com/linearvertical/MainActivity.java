97a526fc3beb5e3072d867dd4db4a5046c3e0cf1
Akhilpoyyayil <akhilpoyyayil92@gmail.com>
2017-03-08 16:24:57 +0530

    application using Linear Layout ( Vertical )

---
+++ LinearVertical/app/src/main/java/calculater/myapp/akhil/com/linearvertical/MainActivity.java
@@ -0,0 +1,13 @@
+package calculater.myapp.akhil.com.linearvertical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
